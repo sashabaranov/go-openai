2e9e4c1bdd03d76b911ecdd4be8c6125c1d127ee
Oleg <97077423+RobotSail@users.noreply.github.com>
2022-08-01 13:05:47 -0400

    test embedding correctness
    Signed-off-by: Oleg <97077423+RobotSail@users.noreply.github.com>

--- api_test.go
+++ api_test.go
@@ -1,7 +1,9 @@
 package gogpt
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"io/ioutil"
 	"testing"
 )
@@ -65,5 +67,44 @@ func TestAPI(t *testing.T) {
 	_, err = c.CreateEmbeddings(ctx, embeddingReq)
 	if err != nil {
 		t.Fatalf("Embedding error: %v", err)
+	}
+}
+
+func TestEmbedding(t *testing.T) {
+	embeddedModels := []EmbeddingModel{
+		AdaSimilarity,
+		BabbageSimilarity,
+		CurieSimilarity,
+		DavinciSimilarity,
+		AdaSearchDocument,
+		AdaSearchQuery,
+		BabbageSearchDocument,
+		BabbageSearchQuery,
+		CurieSearchDocument,
+		CurieSearchQuery,
+		DavinciSearchDocument,
+		DavinciSearchQuery,
+		AdaCodeSearchCode,
+		AdaCodeSearchText,
+		BabbageCodeSearchCode,
+		BabbageCodeSearchText,
+	}
+	for _, model := range embeddedModels {
+		embeddingReq := EmbeddingRequest{
+			Input: []string{
+				"The food was delicious and the waiter",
+				"Other examples of embedding request",
+			},
+			Model: model,
+		}
+		// marshal embeddingReq to JSON and confirm that the model field equals
+		// the AdaSearchQuery type
+		marshaled, err := json.Marshal(embeddingReq)
+		if err != nil {
+			t.Fatalf("Could not marshal embedding request: %v", err)
+		}
+		if !bytes.Contains(marshaled, []byte(`"model":"`+model.String()+`"`)) {
+			t.Fatalf("Expected embedding request to contain model field")
+		}
 	}
 }

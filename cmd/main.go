5f662e2a93d90b27f4fa58d0c030c132433fa937
ealvar3z <55966724+ealvar3z@users.noreply.github.com>
2023-01-24 21:50:21 -0800

    suppress linting errors

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,35 @@
+//nolint:forbidigo // test for a possible user consuming this library
 package main
 
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 func main() {
-	apiKey := os.Getenv("OPENAI_API_KEY")
-	if apiKey == "" {
-		log.Fatalln("Missing API KEY")
-	}
-
-	client := gogpt.NewClient(apiKey)
-
-	fmt.Println("starting stream:")
+	c := gogpt.NewClient(os.Getenv("OPENAI_API_KEY"))
+	ctx := context.Background()
 
 	request := gogpt.CompletionRequest{
-		Prompt:    "Ex falso quodlibet",
-		Model:     "text-davinci-002",
+		Prompt: "Ex falso quodlibet",
+		Model:  "text-davinci-002",
+		//nolint:gomnd // this is test for a user consuming this library
 		MaxTokens: 20,
 		Stream:    true,
 	}
 
-	ctx := context.Background()
-	responses, err := client.CreateCompletionStream(ctx, request)
+	fmt.Println("starting stream:")
+
+	responses, err := c.CreateCompletionStream(ctx, request)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for _, response := range responses {
-		fmt.Println(response.Choices[0].Text)
+	for _, resp := range responses {
+		fmt.Println(resp.Choices[0].Text)
 	}
 }

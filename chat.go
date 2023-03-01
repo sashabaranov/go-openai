d26bfbea7c0d246507a5039a8ec12dfae91181dd
Parham Negahdar <pnegahdar@gmail.com>
2023-03-01 17:51:13 -0500

    Full Model
    Per: platform.openai.com/docs/api-reference/chat/create

--- chat.go
+++ chat.go
@@ -19,8 +19,24 @@ type ChatCompletionMessage struct {
 
 // ChatCompletionRequest represents a request structure for chat completion API.
 type ChatCompletionRequest struct {
-	Model    string                  `json:"model"`
-	Messages []ChatCompletionMessage `json:"messages"`
+	Model            string                  `json:"model"`
+	Messages         []ChatCompletionMessage `json:"messages"`
+	Prompt           string                  `json:"prompt,omitempty"`
+	Suffix           string                  `json:"suffix,omitempty"`
+	MaxTokens        int                     `json:"max_tokens,omitempty"`
+	Temperature      float32                 `json:"temperature,omitempty"`
+	TopP             float32                 `json:"top_p,omitempty"`
+	N                int                     `json:"n,omitempty"`
+	Stream           bool                    `json:"stream,omitempty"`
+	LogProbs         int                     `json:"logprobs,omitempty"`
+	Echo             bool                    `json:"echo,omitempty"`
+	Stop             []string                `json:"stop,omitempty"`
+	PresencePenalty  float32                 `json:"presence_penalty,omitempty"`
+	FrequencyPenalty float32                 `json:"frequency_penalty,omitempty"`
+	BestOf           int                     `json:"best_of,omitempty"`
+	LogitBias        map[string]int          `json:"logit_bias,omitempty"`
+	User             string                  `json:"user,omitempty"`
+	
 }
 
 type ChatCompletionChoice struct {

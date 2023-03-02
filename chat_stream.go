d02d62c1c34c5ec59398a3b6c3ee2ce260ad257c
NICEXAI <nicezhuyunfei@gmail.com>
2023-03-02 19:07:36 +0800

    Implement chat completion streaming

--- chat_stream.go
+++ chat_stream.go
@@ -5,16 +5,27 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"io"
 	"net/http"
 )
 
-var (
-	ErrTooManyEmptyStreamMessages = errors.New("stream has sent too many empty messages")
-)
+type ChatCompletionStreamResponse struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int64  `json:"created"`
+	Model   string `json:"model"`
+	Choices []struct {
+		Index int `json:"index"`
+		Delta struct {
+			Content string `json:"content"`
+		} `json:"delta"`
+		FinishReason string `json:"finish_reason"`
+	} `json:"choices"`
+}
 
-type CompletionStream struct {
+// ChatCompletionStream
+// Note: Perhaps it is more elegant to abstract Stream using generics
+type ChatCompletionStream struct {
 	emptyMessagesLimit uint
 	isFinished         bool
 
@@ -22,7 +33,7 @@ type CompletionStream struct {
 	response *http.Response
 }
 
-func (stream *CompletionStream) Recv() (response CompletionResponse, err error) {
+func (stream *ChatCompletionStream) Recv() (response ChatCompletionStreamResponse, err error) {
 	if stream.isFinished {
 		err = io.EOF
 		return
@@ -59,20 +70,24 @@ waitForData:
 	return
 }
 
-func (stream *CompletionStream) Close() {
+func (stream *ChatCompletionStream) Close() {
 	stream.response.Body.Close()
 }
 
-// CreateCompletionStream — API call to create a completion w/ streaming
+func (stream *ChatCompletionStream) GetResponse() *http.Response {
+	return stream.response
+}
+
+// CreateChatCompletionStream — API call to create a chat completion w/ streaming
 // support. It sets whether to stream back partial progress. If set, tokens will be
 // sent as data-only server-sent events as they become available, with the
 // stream terminated by a data: [DONE] message.
-func (c *Client) CreateCompletionStream(
+func (c *Client) CreateChatCompletionStream(
 	ctx context.Context,
-	request CompletionRequest,
-) (stream *CompletionStream, err error) {
+	request ChatCompletionRequest,
+) (stream *ChatCompletionStream, err error) {
 	request.Stream = true
-	req, err := c.newStreamRequest(ctx, "POST", "/completions", request)
+	req, err := c.newStreamRequest(ctx, "POST", "/chat/completions", request)
 	if err != nil {
 		return
 	}
@@ -82,11 +97,10 @@ func (c *Client) CreateCompletionStream(
 		return
 	}
 
-	stream = &CompletionStream{
+	stream = &ChatCompletionStream{
 		emptyMessagesLimit: c.config.EmptyMessagesLimit,
-
-		reader:   bufio.NewReader(resp.Body),
-		response: resp,
+		reader:             bufio.NewReader(resp.Body),
+		response:           resp,
 	}
 	return
 }

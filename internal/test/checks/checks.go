061446956a0a4db5d60047d62509d826b30af3b8
Rex Posadas <rposadas@redwoodlogistics.com>
2023-03-22 02:15:31 +0800

    corrected NoError

--- internal/test/checks/checks.go
+++ internal/test/checks/checks.go
@@ -4,31 +4,9 @@ import (
 	"testing"
 )
 
-//func NoError(t *testing.T, err error, message ...string) {
-//
-//	pc, file, lineNo, ok := runtime.Caller(1)
-//	if !ok {
-//		return
-//	}
-//	funcName := runtime.FuncForPC(pc).Name()
-//	fileName := path.Base(file) // The Base function returns the last element of the path
-//
-//	loc := fmt.Sprintf("FuncName:%s, file:%s, line:%d ", funcName, fileName, lineNo)
-//
-//	if err == nil {
-//		t.Errorf(loc, message)
-//	}
-//}
-
-//func NoError(t *testing.T, err error, message ...string) {
-//	if err != nil {
-//		t.Error(message)
-//	}
-//}
-
 func NoError(t *testing.T, err error, message ...string) {
 	t.Helper()
-	if err == nil {
+	if err != nil {
 		t.Error(err, message)
 	}
 }

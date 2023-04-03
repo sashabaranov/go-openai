f5c822da563f00ffec40d9d80f210efc5d5ef859
ttyS3 <ttys3.rust@gmail.com>
2023-04-04 01:30:13 +0800

    test: add api_internal_test.go

---
+++ api_internal_test.go
@@ -0,0 +1,18 @@
+package openai
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAzureFullURL(t *testing.T) {
+	az := DefaultAzure("dummy", "https://httpbin.org/", "chatgpt-demo")
+	cli := NewClientWithConfig(az)
+	// /openai/deployments/{engine}/chat/completions?api-version={api_version}
+	expect := fmt.Sprintf("https://httpbin.org/openai/deployments/chatgpt-demo/chat/completions?api-version=2023-03-15-preview")
+	actual := cli.fullURL("/chat/completions")
+	if actual != expect {
+		t.Errorf("Expected %s, got %s", expect, actual)
+	}
+	t.Logf("Full URL: %s", actual)
+}

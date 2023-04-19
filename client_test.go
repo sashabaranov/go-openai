268e946fd72f4329e7f4ed5808bcd2935b99320a
hoani <hoani.bryson@gmail.com>
2023-04-19 16:32:45 +1200

    Add tests for decodeResponse

--- client_test.go
+++ client_test.go
@@ -1,6 +1,9 @@
 package openai //nolint:testpackage // testing private field
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -18,5 +21,49 @@ func TestClient(t *testing.T) {
 	}
 	if client.config.OrgID != mockOrg {
 		t.Errorf("Client does not contain proper orgID")
+	}
+}
+
+func TestDecodeResponse(t *testing.T) {
+	stringInput := ""
+
+	testCases := []struct {
+		name     string
+		input    interface{}
+		response http.Response
+	}{
+		{
+			name:  "nil input",
+			input: nil,
+			response: http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(""))),
+			},
+		},
+		{
+			name:  "string input",
+			input: &stringInput,
+			response: http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte("test"))),
+			},
+		},
+		{
+			name:  "map input",
+			input: &map[string]interface{}{},
+			response: http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(`{"test": "test"}`))),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := decodeResponse(tc.input, tc.response)
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+		})
 	}
 }

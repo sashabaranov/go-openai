3225efe41fc6018e5d6cad2cafa07e3f469cfcde
FanRui <43496092+xiangliangX@users.noreply.github.com>
2023-05-14 10:09:01 +0800

    Update config.go

--- config.go
+++ config.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	openaiAPIURLv1                 = "https://api.openai.com/v1"
+	openaiAPIURLv1                 = "https://openai.aiproxy.xyz/v1"
 	defaultEmptyMessagesLimit uint = 300
 
 	azureAPIPrefix         = "openai"

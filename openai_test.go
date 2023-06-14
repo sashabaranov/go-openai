27cc5caa808e8a4501400f3e41f44ffe00d8f4dc
coggsflod <richard.coggins@officedepot.com>
2023-06-13 21:15:17 -0400

    Remove TestAzureChatCompletionsWithCustomDeploymentName

--- openai_test.go
+++ openai_test.go
@@ -26,25 +26,3 @@ func setupAzureTestServer() (client *Client, server *test.ServerTest, teardown f
 	client = NewClientWithConfig(config)
 	return
 }
-
-func setupAzureTestServerWithCustomDeploymentName() (client *Client, server *test.ServerTest, teardown func()) {
-	server = test.NewTestServer()
-	ts := server.OpenAITestServer()
-	ts.Start()
-	teardown = ts.Close
-	config := DefaultAzureConfig(test.GetTestToken(), "https://dummylab.openai.azure.com/")
-	config.BaseURL = ts.URL
-	config.AzureModelMapperFunc = func(model string) string {
-		azureModelMapping := map[string]string{
-			"gpt-3.5-turbo":      "custom-gpt-3.5-turbo",
-			"gpt-3.5-turbo-0301": "custom-gpt-3.5-turbo-03-01",
-			"gpt-4":              "custom-gpt-4",
-			"gpt-4-0314":         "custom-gpt-4-03-14",
-			"gpt-4-32k":          "custom-gpt-4-32k",
-			"gpt-4-32k-0314":     "custom-gpt-4-32k-03-14",
-		}
-		return azureModelMapping[model]
-	}
-	client = NewClientWithConfig(config)
-	return
-}

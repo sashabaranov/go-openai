57b824b9aeb9cfe813847f38233f3354a9310434
vvatanabe <yuichi.watanabe.ja@gmail.com>
2023-06-22 05:04:32 +0900

    refactor: Change the style of HTTPRequestBuilder.Build func to one-argument-per-line.

--- internal/request_builder.go
+++ internal/request_builder.go
@@ -21,8 +21,13 @@ func NewRequestBuilder() *HTTPRequestBuilder {
 	}
 }
 
-func (b *HTTPRequestBuilder) Build(ctx context.Context,
-	method, url string, body any, header http.Header) (req *http.Request, err error) {
+func (b *HTTPRequestBuilder) Build(
+	ctx context.Context,
+	method string,
+	url string,
+	body any,
+	header http.Header,
+) (req *http.Request, err error) {
 	var bodyReader io.Reader
 	if body != nil {
 		if v, ok := body.(io.Reader); ok {

bd1bb2065aedd6295445d8e67310d09425e09a7c
e. alvarez <55966724+ealvar3z@users.noreply.github.com>
2023-09-14 17:34:28 -0400

    Rewrite Cryptographically Secure RandomString Function
    - Avoid big package
    - Use strings.Builder for efficient string concatenation
    - Set default string length to 10
    - Handle errors from rand.Read()
    
    Resolves: #487

--- internal/test/random.go
+++ internal/test/random.go
@@ -2,34 +2,42 @@ package test
 
 import (
 	"crypto/rand"
-	"math/big"
+	"strings"
 )
 
-var (
-	strLen = 10
-	//nolint:gomnd // this avoids the golangci-lint "magic number" warning
-	n = big.NewInt(2)
-)
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const strLen = 10
 
-// RandomString generates a cryptographically secure random string of a fixed
-// length. The string is composed of alphanumeric characters and is generated
-// using the crypto/rand library. The length of the string is determined by the
-// constant strLen.
+// See StackOverflow answer:
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+// RandomString generates a cryptographically secure random string of length
+// strLen.
 func RandomString() string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	s := make([]rune, strLen)
-	max := big.NewInt(int64(len(letters)))
-	for i := range s {
-		randomInt, _ := rand.Int(rand.Reader, max)
-		s[i] = letters[randomInt.Int64()]
+	sb := strings.Builder{}
+	sb.Grow(strLen)
+
+	for i := 0; i < strLen; i++ {
+		randomByte := make([]byte, 1)
+		_, err := rand.Read(randomByte)
+		if err != nil {
+			return ""
+		}
+		randomIndex := randomByte[0] % byte(len(letters))
+		sb.WriteByte(letters[randomIndex])
 	}
-	return string(s)
+
+	return sb.String()
 }
 
-// RandomBool generates a cryptographically secure random boolean value. It
-// uses the crypto/rand library to generate a random integer (either 0 or 1),
-// and returns true if the integer is 1, and false otherwise.
+// RandomBool generates a cryptographically secure random boolean value.
+// It reads a single byte from the crypto/rand library and uses its least
+// significant bit to determine the boolean value. The function returns
+// true if the least significant bit is 1, and false otherwise.
 func RandomBool() bool {
-	randomInt, _ := rand.Int(rand.Reader, n)
-	return randomInt.Int64() == 1
+	var b [1]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		return false
+	}
+	return b[0]&1 == 1
 }

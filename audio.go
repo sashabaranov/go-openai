sashabaranov/go-openai

commit 8e165dc9aadc9f7045b91dd1b02d6404940dc023

Feat Add headers to openai responses (#506)
* feat: add headers to http response

* chore: add test

* fix: rename to httpHeader

audio.go
@@ -63,6 +63,21 @@ type AudioResponse struct {
 		Transient        bool    `json:"transient"`
 	} `json:"segments"`
 	Text string `json:"text"`
+
+	httpHeader
+}
+
+type audioTextResponse struct {
+	Text string `json:"text"`
+
+	httpHeader
+}
+
+func (r *audioTextResponse) ToAudioResponse() AudioResponse {
+	return AudioResponse{
+		Text:       r.Text,
+		httpHeader: r.httpHeader,
+	}
 }
 
 // CreateTranscription — API call to create a transcription. Returns transcribed text.
@@ -104,7 +119,9 @@ func (c *Client) callAudioAPI(
 	if request.HasJSONResponse() {
 		err = c.sendRequest(req, &response)
 	} else {
-		err = c.sendRequest(req, &response.Text)
+		var textResponse audioTextResponse
+		err = c.sendRequest(req, &textResponse)
+		response = textResponse.ToAudioResponse()
 	}
 	if err != nil {
 		return AudioResponse{}, err

client.go
@@ -20,6 +20,20 @@ type Client struct {
 	createFormBuilder func(io.Writer) utils.FormBuilder
 }
 
+type Response interface {
+	SetHeader(http.Header)
+}
+
+type httpHeader http.Header
+
+func (h *httpHeader) SetHeader(header http.Header) {
+	*h = httpHeader(header)
+}
+
+func (h httpHeader) Header() http.Header {
+	return http.Header(h)
+}
+
 // NewClient creates new OpenAI API client.
 func NewClient(authToken string) *Client {
 	config := DefaultConfig(authToken)
@@ -82,7 +96,7 @@ func (c *Client) newRequest(ctx context.Context, method, url string, setters ...
 	return req, nil
 }
 
-func (c *Client) sendRequest(req *http.Request, v any) error {
+func (c *Client) sendRequest(req *http.Request, v Response) error {
 	req.Header.Set("Accept", "application/json; charset=utf-8")
 
 	// Check whether Content-Type is already set, Upload Files API requires
@@ -101,6 +115,10 @@ func (c *Client) sendRequest(req *http.Request, v any) error {
 
 	if isFailureStatusCode(res) {
 		return c.handleErrorResp(res)
+	}
+
+	if v != nil {
+		v.SetHeader(res.Header)
 	}
 
 	return decodeResponse(res.Body, v)

chat.go
@@ -142,6 +142,8 @@ type ChatCompletionResponse struct {
 	Model   string                 `json:"model"`
 	Choices []ChatCompletionChoice `json:"choices"`
 	Usage   Usage                  `json:"usage"`
+
+	httpHeader
 }
 
 // CreateChatCompletion — API call to Create a completion for the chat message.

chat_test.go
@@ -16,6 +16,11 @@ import (
 	"github.com/sashabaranov/go-openai/jsonschema"
 )
 
+const (
+	xCustomHeader      = "X-CUSTOM-HEADER"
+	xCustomHeaderValue = "test"
+)
+
 func TestChatCompletionsWrongModel(t *testing.T) {
 	config := DefaultConfig("whatever")
 	config.BaseURL = "http://localhost/v1"
@@ -66,6 +71,30 @@ func TestChatCompletions(t *testing.T) {
 		},
 	})
 	checks.NoError(t, err, "CreateChatCompletion error")
+}
+
+// TestCompletions Tests the completions endpoint of the API using the mocked server.
+func TestChatCompletionsWithHeaders(t *testing.T) {
+	client, server, teardown := setupOpenAITestServer()
+	defer teardown()
+	server.RegisterHandler("/v1/chat/completions", handleChatCompletionEndpoint)
+	resp, err := client.CreateChatCompletion(context.Background(), ChatCompletionRequest{
+		MaxTokens: 5,
+		Model:     GPT3Dot5Turbo,
+		Messages: []ChatCompletionMessage{
+			{
+				Role:    ChatMessageRoleUser,
+				Content: "Hello!",
+			},
+		},
+	})
+	checks.NoError(t, err, "CreateChatCompletion error")
+
+	a := resp.Header().Get(xCustomHeader)
+	_ = a
+	if resp.Header().Get(xCustomHeader) != xCustomHeaderValue {
+		t.Errorf("expected header %s to be %s", xCustomHeader, xCustomHeaderValue)
+	}
 }
 
 // TestChatCompletionsFunctions tests including a function call.
@@ -281,6 +310,7 @@ func handleChatCompletionEndpoint(w http.ResponseWriter, r *http.Request) {
 		TotalTokens:      inputTokens + completionTokens,
 	}
 	resBytes, _ = json.Marshal(res)
+	w.Header().Set(xCustomHeader, xCustomHeaderValue)
 	fmt.Fprintln(w, string(resBytes))
 }
 

completion.go
@@ -154,6 +154,8 @@ type CompletionResponse struct {
 	Model   string             `json:"model"`
 	Choices []CompletionChoice `json:"choices"`
 	Usage   Usage              `json:"usage"`
+
+	httpHeader
 }
 
 // CreateCompletion — API call to create a completion. This is the main endpoint of the API. Returns new text as well

edits.go
@@ -28,6 +28,8 @@ type EditsResponse struct {
 	Created int64         `json:"created"`
 	Usage   Usage         `json:"usage"`
 	Choices []EditsChoice `json:"choices"`
+
+	httpHeader
 }
 
 // Edits Perform an API call to the Edits endpoint.

embeddings.go
@@ -150,6 +150,8 @@ type EmbeddingResponse struct {
 	Data   []Embedding    `json:"data"`
 	Model  EmbeddingModel `json:"model"`
 	Usage  Usage          `json:"usage"`
+
+	httpHeader
 }
 
 type base64String string
@@ -182,6 +184,8 @@ type EmbeddingResponseBase64 struct {
 	Data   []Base64Embedding `json:"data"`
 	Model  EmbeddingModel    `json:"model"`
 	Usage  Usage             `json:"usage"`
+
+	httpHeader
 }
 
 // ToEmbeddingResponse converts an embeddingResponseBase64 to an EmbeddingResponse.

engines.go
@@ -12,11 +12,15 @@ type Engine struct {
 	Object string `json:"object"`
 	Owner  string `json:"owner"`
 	Ready  bool   `json:"ready"`
+
+	httpHeader
 }
 
 // EnginesList is a list of engines.
 type EnginesList struct {
 	Engines []Engine `json:"data"`
+
+	httpHeader
 }
 
 // ListEngines Lists the currently available engines, and provides basic

commit 687f7d5f71e95e81f0481093f28acaabdc27f238

Fix support for non json speech to text
Fixes issue when using non audio formats:

Transcription error: json: cannot unmarshal number into Go value of type openai.audioTextResponse

Fixes: 8e165dc9aadc ("Feat Add headers to openai responses (#506)")

audio.go
@@ -68,9 +68,13 @@ type AudioResponse struct {
 }
 
 type audioTextResponse struct {
-	Text string `json:"text"`
+	Text string
 
 	httpHeader
+}
+
+func (r *audioTextResponse) SetText(text string) {
+	r.Text = text
 }
 
 func (r *audioTextResponse) ToAudioResponse() AudioResponse {

client.go
@@ -24,6 +24,10 @@ type Response interface {
 	SetHeader(http.Header)
 }
 
+type TextResponse interface {
+	SetText(string)
+}
+
 type httpHeader http.Header
 
 func (h *httpHeader) SetHeader(header http.Header) {
@@ -195,6 +199,13 @@ func decodeResponse(body io.Reader, v any) error {
 
 	if result, ok := v.(*string); ok {
 		return decodeString(body, result)
+	} else if result, ok := v.(TextResponse); ok {
+		var text string
+		if err := decodeString(body, &text); err != nil {
+			return err
+		}
+		result.SetText(text)
+		return nil
 	}
 	return json.NewDecoder(body).Decode(v)
 }
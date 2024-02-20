b20eb0dec2ad708b7804817208a1f46c2f25d980
Rich Coggins <Richard.Coggins@TheODPCorp.com>
2024-02-20 17:30:48 -0500

    fix:fix url for Azure assistants api

--- client.go
+++ client.go
@@ -221,7 +221,7 @@ func (c *Client) fullURL(suffix string, args ...any) string {
 		baseURL = strings.TrimRight(baseURL, "/")
 		// if suffix is /models change to {endpoint}/openai/models?api-version=2022-12-01
 		// https://learn.microsoft.com/en-us/rest/api/cognitiveservices/azureopenaistable/models/list?tabs=HTTP
-		if strings.Contains(suffix, "/models") {
+		if strings.Contains(suffix, "/models") || strings.Contains(suffix, "/assistants") {
 			return fmt.Sprintf("%s/%s%s?api-version=%s", baseURL, azureAPIPrefix, suffix, c.config.APIVersion)
 		}
 		azureDeploymentName := "UNKNOWN"

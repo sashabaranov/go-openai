6b7af61f3a5e1c0ebf62e66415e969bfa2b6a360
push edp <aspushedp@gmail.com>
2024-03-28 05:09:31 +1000

    feat: support for Assistant stream mode and implemented stream event callbacks

---
+++ assistant_stream.go
@@ -0,0 +1,79 @@
+package openai
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+/*
+	type AssistantThreadRunStreamDelta struct {
+		Content      string        `json:"content,omitempty"`
+		Role         string        `json:"role,omitempty"`
+	}
+*/
+type AssistantThreadRunStreamResponse struct {
+	ID     string `json:"id"`
+	Object string `json:"object"`
+}
+
+type AssistantThreadRunStream struct {
+	*streamReader[AssistantThreadRunStreamResponse]
+}
+
+func (c *Client) CreateAssistantThreadRunStream(
+	ctx context.Context,
+	threadID string,
+	request RunRequest,
+) (stream *AssistantThreadRunStream, err error) {
+	request.Stream = true
+	urlSuffix := fmt.Sprintf("/threads/%s/runs", threadID)
+	req, err := c.newRequest(
+		ctx,
+		http.MethodPost,
+		c.fullURL(urlSuffix),
+		withBody(request),
+		withBetaAssistantV1(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := sendRequestStream[AssistantThreadRunStreamResponse](c, req)
+	if err != nil {
+		return nil, err
+	}
+	stream = &AssistantThreadRunStream{
+		streamReader: resp,
+	}
+	return
+}
+
+func (c *Client) CreateAssistantThreadRunSubmitToolOutputStream(
+	ctx context.Context,
+	threadID string,
+	runID string,
+	request SubmitToolOutputsRequest,
+) (stream *AssistantThreadRunStream, err error) {
+	request.Stream = true
+	urlSuffix := fmt.Sprintf("/threads/%s/runs/%s/submit_tool_outputs", threadID, runID)
+	req, err := c.newRequest(
+		ctx,
+		http.MethodPost,
+		c.fullURL(urlSuffix),
+		withBody(request),
+		withBetaAssistantV1(),
+	)
+	if err != nil {
+		return
+	}
+
+	resp, err := sendRequestStream[AssistantThreadRunStreamResponse](c, req)
+	if err != nil {
+		return nil, err
+	}
+	stream = &AssistantThreadRunStream{
+		streamReader: resp,
+	}
+	return
+}

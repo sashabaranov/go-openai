432d878d247150f213576bb00309b790831adcb8
Arash Khazraie <akhazr@gmail.com>
2024-06-21 12:07:25 -0700

    adds min_p

--- completion.go
+++ completion.go
@@ -131,6 +131,7 @@ type CompletionRequest struct {
 	MaxTokens        int      `json:"max_tokens,omitempty"`
 	Temperature      float32  `json:"temperature,omitempty"`
 	TopP             float32  `json:"top_p,omitempty"`
+	MinP             float32  `json:"min_p,omitempty"`
 	N                int      `json:"n,omitempty"`
 	Stream           bool     `json:"stream,omitempty"`
 	LogProbs         int      `json:"logprobs,omitempty"`

fdd59d93413154cd07b2e46a428b15eda40b26e2
Liu Shuang <liushuangbill@outlook.com>
2024-09-26 18:30:56 +0800

    feat: usage struct add CompletionTokensDetails (#863)

--- common.go
+++ common.go
@@ -4,7 +4,13 @@ package openai
 
 // Usage Represents the total token usage per request to OpenAI.
 type Usage struct {
-	PromptTokens     int `json:"prompt_tokens"`
-	CompletionTokens int `json:"completion_tokens"`
-	TotalTokens      int `json:"total_tokens"`
+	PromptTokens            int                      `json:"prompt_tokens"`
+	CompletionTokens        int                      `json:"completion_tokens"`
+	TotalTokens             int                      `json:"total_tokens"`
+	CompletionTokensDetails *CompletionTokensDetails `json:"completion_tokens_details"`
+}
+
+// CompletionTokensDetails Breakdown of tokens used in a completion.
+type CompletionTokensDetails struct {
+	ReasoningTokens int `json:"reasoning_tokens"`
 }

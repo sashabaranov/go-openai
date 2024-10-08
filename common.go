c85f781a2ba64d1fbb458aade358ab95994ac4a9
Isaac Seymour <i.seymour@oxon.org>
2024-10-08 16:27:28 +0100

    Add cached and audio tokens to usage structs
    These have been added to the completions API recently:
    https://platform.openai.com/docs/api-reference/chat/object#chat/object-usage

--- common.go
+++ common.go
@@ -7,10 +7,18 @@ type Usage struct {
 	PromptTokens            int                      `json:"prompt_tokens"`
 	CompletionTokens        int                      `json:"completion_tokens"`
 	TotalTokens             int                      `json:"total_tokens"`
+	PromptTokensDetails     *PromptTokensDetails     `json:"prompt_tokens_details"`
 	CompletionTokensDetails *CompletionTokensDetails `json:"completion_tokens_details"`
 }
 
 // CompletionTokensDetails Breakdown of tokens used in a completion.
 type CompletionTokensDetails struct {
+	AudioTokens     int `json:"audio_tokens"`
 	ReasoningTokens int `json:"reasoning_tokens"`
+}
+
+// PromptTokensDetails Breakdown of tokens used in the prompt.
+type PromptTokensDetails struct {
+	AudioTokens  int `json:"audio_tokens"`
+	CachedTokens int `json:"cached_tokens"`
 }

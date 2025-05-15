959df8c294397a8e292d661ed6587c594ce7bc2c
maye.chy <chenhongyu@come-future.com>
2025-05-15 18:17:07 +0800

    golint

--- completion_test.go
+++ completion_test.go
@@ -319,7 +319,7 @@ func TestRegisterSupportsModel(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(_ *testing.T) {
 			openai.RegisterSupportsModel(tt.args.endpoint, tt.args.model)
 		})
 	}
